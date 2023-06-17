91c92a729c18671edc1171a3088d6d0ecf806b89
Janay Ma <janay.ma7@gmail.com>
2023-06-16 19:04:53 -0700

    fetch coordinates from ipwhois with given ip address

--- iss.js
+++ iss.js
@@ -3,25 +3,50 @@ const request = require('request');
 
 
 const fetchMyIP = (callback) => {
-  console.log('requesting...');
+  console.log('requesting IP ...');
   request('https://api64.ipify.org?format=json', (error, response, body) => {
+    //handle errors
     if (error) {
       callback(error, null);
       return;
     }
+    //handle non successful status codes
     if (response.statusCode !== 200) {
       console.log(response);
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
+    //ip found!
     const ip = JSON.parse(body).ip;
     callback(error, ip);
   });
 };
 
-// fetchMyIP((error, ip) => {
-//   console.log(ip);
-// });
+const fetchCoordsByIP = (ip, callback) => {
+  console.log('requesting coordinates ...');
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
 
-module.exports = { fetchMyIP };
+    const bodyObj = JSON.parse(body);
+
+    if (!bodyObj.success) {
+      const message = `Success status was ${bodyObj.success}. Server message says: ${bodyObj.message} when fetching for IP ${bodyObj.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+
+
+    const { latitude, longitude } = bodyObj;
+
+    callback(error, { latitude, longitude });
+  });
+
+
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
